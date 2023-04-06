9d2d6b70aa91e6ee57b9a9e35be7c9c7706b77ac
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-06 16:46:25 +0300

    3. Fun "dataScience" - enter data from User and Computer

--- game0X_GitHub.cpp
+++ game0X_GitHub.cpp
@@ -3,7 +3,32 @@
 #include<Windows.h>
 using namespace std;
 
-//Написать игру «Крестики-нолики»
+//Write a "tic-tac-toe" game
+void dataScience(char desk[3][3])
+{
+	cout << "Desk for game \"X\" & \"Y\"\n\n";
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			cout << "[" << desk[i][j] << "]";
+		cout << endl;
+	}
+
+	cout << "\nUser, enter position of X & Y (1...3): ";
+	int x, y;//user coordinates
+	cin >> x >> y;
+
+	cout << "\nComputer, enter position of X & Y (1...3): ";
+	int x2, y2;//comp coordinates
+	do
+	{
+		x2 = rand() % 3;
+		y2 = rand() % 3;
+	} while (desk[x2][y2] != '-'); //checking of occupied position for computer
+	desk[x2][y2] = 'o';
+
+	cout << endl;
+}
 
 int main()
 {
@@ -16,6 +41,9 @@ int main()
 	for (int i = 0; i < 3; i++) // fill the desk
 		for (int j = 0; j < 3; j++)
 			desk[i][j] = '-';
+
+	//Function
+	dataScience(desk);
 
 	return 0;
 }

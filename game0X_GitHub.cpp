9d5345441a3499a052529f1c771a9b50168aa369
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-06 16:27:52 +0300

    Fill array 3x3 for desk

--- game0X_GitHub.cpp
+++ game0X_GitHub.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include<string>
+#include<Windows.h>
 using namespace std;
+
+//Написать игру «Крестики-нолики»
 
 int main()
 {
 	//Start point
+	//x - user
+	//o - compture
+	srand(time(0));
+	char desk[3][3]{};
+
+	for (int i = 0; i < 3; i++) // fill the desk
+		for (int j = 0; j < 3; j++)
+			desk[i][j] = '-';
 
 	return 0;
 }

8ec1f8ed627cec7b1a9568629d396cee535bfc94
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-06 17:30:21 +0300

    6. Fun "gameWin" - added logic for check Winner and stop game

--- game0X_GitHub.cpp
+++ game0X_GitHub.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 //Write a "tic-tac-toe" game
+
+bool gameWin(char desk[3][3], char s);
+
 void dataScience(char desk[3][3])
 {
 	int count = 0;
@@ -46,6 +49,9 @@ void dataScience(char desk[3][3])
 
 		desk[x - 1][y - 1] = 'x';
 
+		if (gameWin(desk, 'x'))
+			break;
+
 		//counter of step for stop game
 		count++;
 		if (count == pow(3, 2))
@@ -61,10 +67,44 @@ void dataScience(char desk[3][3])
 			y2 = rand() % 3;
 		} while (desk[x2][y2] != '-'); //checking of occupied position for computer
 		desk[x2][y2] = 'o';
+
+		if (gameWin(desk, 'o'))
+			break;
+
 		//counter of step for stop game
 		count++;
 
 	} while (true);
+}
+
+bool gameWin(char desk[3][3], char s)
+{
+	//gameWin
+	int w0 = 0, w1 = 0, w2 = 0, w3 = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (desk[i][j] == s)//matches by horizontal lines
+				w0++;
+
+			if (desk[j][i] == s)//matches by vertical lines
+				w1++;
+
+			if (desk[j][j] == s)//matches by diagonal line from left up to right down
+				w2++;
+
+			if (desk[2 - j][j] == s)//opposite matches by diagonal line from right up to left down
+				w3++;
+		}
+		if (w0 == 3 || w1 == 3 || w2 == 3 || w3 == 3)
+		{
+			return true;
+			break;
+		}
+		w0 = 0, w1 = 0, w2 = 0, w3 = 0;
+	}
+	return false;
 }
 
 int main()
